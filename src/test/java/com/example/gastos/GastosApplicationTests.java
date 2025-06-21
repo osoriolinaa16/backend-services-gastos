494cc9b2bec0a150e8d815eed50398b4a60d40b1
osoriolinaa16 <osoriolinaa16@gmail.com>
2025-06-20 21:51:12 -0500

    Initialize Spring Boot project

---
+++ src/test/java/com/example/gastos/GastosApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
